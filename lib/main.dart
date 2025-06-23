19da6f402ed2a1ac3afc3831547ee50c2ac73b9a
Adarsh Singh <adarsh.this.side@gmail.com>
2025-06-23 16:20:36 +0530

    Refactor main function to initialize Firebase and set up app theme and provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,15 @@
+import 'package:demo_app/config/app_theme.dart';
+import 'package:demo_app/firebase_options.dart';
+import 'package:demo_app/provider/home_provider.dart';
+import 'package:demo_app/views/screens/home_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MainApp());
 }
 
@@ -9,8 +18,23 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(body: Center(child: Text('Hello World!'))),
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+    return AnnotatedRegion(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        statusBarIconBrightness: Brightness.dark,
+        systemNavigationBarColor: Colors.white,
+        systemNavigationBarIconBrightness: Brightness.dark,
+      ),
+      child: ChangeNotifierProvider(
+        create: (context) => HomeProvider(),
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: "Salon Booking App",
+          theme: AppTheme.lightTheme,
+          home: HomeScreen(),
+        ),
+      ),
     );
   }
 }
